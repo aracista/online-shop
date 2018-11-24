199e443abf00f3bea3da7643f444f93fe67478ba
Ian rizki <ian.rizki2@gmail.com>
2018-11-24 17:09:53 +0700

    shopping cart fix

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.main')
 
 @section('content')
+<div class="row">
 	<h3>Cart Item</h3>
 
 	<table class="table table-hover">
@@ -10,6 +11,7 @@
 				<th>Harga</th>
 				<th>Qty</th>
 				<th>Warna</th>
+				<th>Action</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -37,16 +39,26 @@
 					</form>
 				</td>  
 				<td>{{$cartItem->attributes->has('warna')?$cartItem->attributes->warna:''}}</td>
+				<td>
+					<form action="{{route('cart.destroy',$cartItem->id)}}" method="POST" role="form">
+						@csrf
+						@method('DELETE')
+						<button type="submit" class="button btn-danger" value="Delete">Delete</button>
+					</form>
+				</td>
 			</tr>
 			@endforeach
 
 			<tr>
 				<td>Total</td>
-				<td>Rp&nbsp;{{Cart::getTotal()}}</td>
+				<td>
+				Rp&nbsp;{{Cart::getTotal()}}
+				</td>
 				<td>{{Cart::getTotalQuantity()}}</td>
 			</tr>
 		</tbody>
 	</table>
 	<a href="" class="button">Checkout</a>
+</div>
 
 @endsection
